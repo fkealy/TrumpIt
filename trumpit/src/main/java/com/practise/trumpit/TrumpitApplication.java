719436b8afecea192e0b1753e03ebb4f7a827d48
fkealy <freddiekealy@outlook.com>
2018-10-17 18:05:16 +0100

    update gitignore and add springboot

---
+++ trumpit/src/main/java/com/practise/trumpit/TrumpitApplication.java
@@ -0,0 +1,12 @@
+package com.practise.trumpit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrumpitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrumpitApplication.class, args);
+	}
+}
---
+++ trumpit/src/test/java/com/practise/trumpit/TrumpitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.practise.trumpit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrumpitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
